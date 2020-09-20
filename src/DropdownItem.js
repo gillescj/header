bb63cddcdb1f8fe5569ce1cab182f4ed3573331e
Colin Gillespie <ratchetandcolin@gmail.com>
2020-09-20 12:14:01 -0400

    Alter dropdown style - minimalistic

--- src/DropdownItem.js
+++ src/DropdownItem.js
@@ -6,14 +6,8 @@ const Container = styled.div`
     display: flex;
     line-height: 1.5rem;
     font-size: 1rem;
-    border: black solid 2px;
     border-radius: 10px;
-    transition: background 700ms;
-    padding: 2rem 1rem;
-
-    &:hover {
-        background: ${COLOURS.darkGrey};
-    }
+    padding: 0.8rem;
 `;
 
 const DropdownItem = ({ children }) => {
--- src/NavItemDropdown.js
+++ src/NavItemDropdown.js
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 
 const HoverWrapper = styled.div`
     position: absolute;
-    border: black dotted 2px;
     padding: 1rem;
     transform: translateX(-20px);
 `;
@@ -14,9 +13,11 @@ const Container = styled.div`
     grid-gap: 3px;
     padding: 1rem;
     color: black;
-    background: ${COLOURS.veryDarkBlue};
-    border: black solid 2px;
-    border-radius: 10px;
+    background: ${COLOURS.bgHeader};
+    border-radius: 5px;
+    box-shadow: 0 0px 2.2px rgba(0, 0, 0, 0.02), 0 0px 5.3px rgba(0, 0, 0, 0.028),
+        0 0px 10px rgba(0, 0, 0, 0.035), 0 0px 17.9px rgba(0, 0, 0, 0.042),
+        0 0px 33.4px rgba(0, 0, 0, 0.05), 0 0px 80px rgba(0, 0, 0, 0.07);
     opacity: ${({ visible }) => (visible ? '1' : '0')};
     transition: opacity 2s ease-in-out;
 `;
